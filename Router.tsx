912e9fbea533efa740708d591a33f8051538bdc5
Avto <atukajiquridze@gmail.com>
2025-05-27 20:59:34 +0400

    feature: added layouts

--- Router.tsx
+++ Router.tsx
@@ -1,13 +1,17 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./src/pages/home/Home";
 import ImagePage from "./src/pages/image/ImagePage";
+import MainLayout from "./src/layouts/MainLayout";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
-        <Route path="images/:id" element={<ImagePage />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="image" element={<ImagePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return <nav>111</nav>;
+};
+
+export default Navbar;
