15bd7f165a6d54a34de29c074a6d69498a5fbaca
Avto <atukajiquridze@gmail.com>
2025-05-27 21:39:37 +0400

    feature : added navbar

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Home = () => {
-  return <div>Home</div>;
+const Home: React.FC = () => {
+  return <div className="text-red-400">Home</div>;
 };
 
 export default Home;
